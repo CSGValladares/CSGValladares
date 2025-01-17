3ca7abf07c422322f1ab8afae8b2734850b9bf22
CSG_Valladares_5 <112735939+CSGValladares@users.noreply.github.com>
2025-01-16 19:24:51 -0600

    Initial commit

---
+++ tienda/comisiones.js
@@ -0,0 +1,8 @@
+function facebook(){
+    location.assign("https://www.facebook.com/profile.php?id=100095413275622");
+}
+
+function whats(){
+    location.assign("https://wa.me/qr/GKP6VQE3LSRYI1");
+}
+
